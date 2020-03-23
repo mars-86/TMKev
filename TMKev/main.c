6e4c4450c601a25a62ae137abf6644daa3e2fced
mars-86 <hrslash86@gmail.com>
2020-03-23 18:08:31 -0300

    refac: Event dispatcher keyb

--- TMKev/main.c
+++ TMKev/main.c
@@ -24,53 +24,45 @@ int main(int argc, char *argv[])
 LRESULT CALLBACK term_proc(HANDLE h, UINT e, WPARAM w, LPARAM l)
 {
     if (TMKEV_GET_EVENT_TYPE(e) == KEY_EVENT) {
-        switch (w) {
-        case VK_ESCAPE:
-            printf("%s", "ESC");
-            break;
-        case VK_KEY_W_LOW:
-            printf("%s", "w");
-            break;
-        default:
-            ;
-            //printf("%d", l);
+        if (TMKEV_KEYB_EVENT_IS_KEY_PRESSED(e)) {
+            switch (TMKEV_KEYB_GET_KEY_AS_ASCII(l)) {
+            case VK_ESCAPE:
+                printf("%s", "ESC");
+                break;
+            case VK_KEY_W_LOW:
+                printf("%s", "w");
+                break;
+            default:
+                ;
+            }
         }
     }
     else if (TMKEV_GET_EVENT_TYPE(e) == MOUSE_EVENT) {
-        switch (TMKEV_GET_EVENT_STYPE(e)) {
-            case MOUSE_MOVED:
-                printf("%d,", TMKEV_MOUSE_GET_X_COORD(l));
-                printf("%d", TMKEV_MOUSE_GET_Y_COORD(l));
+        switch (TMKEV_MOUSE_EVENT_STYPE(e)) {
+        case MOUSE_MOVED:
+            printf("MM: %d,%d,", TMKEV_MOUSE_GET_X_COORD(l), TMKEV_MOUSE_GET_Y_COORD(l));
+            break;
+        case DOUBLE_CLICK:
+            printf("DC: %d,%d,", TMKEV_MOUSE_GET_X_COORD(l), TMKEV_MOUSE_GET_Y_COORD(l));
+            break;
+        case MOUSE_WHEELED:
+            printf("%s", "Wheel");
+            break;
+        // single button pressed
+        default:
+            switch (TMKEV_GET_MOUSE_BUTTON_STATUS(w)) {
+            case VK_LBUTTON:
+                printf("LB: %d,%d,", TMKEV_MOUSE_GET_X_COORD(l), TMKEV_MOUSE_GET_Y_COORD(l));
                 break;
-            case DOUBLE_CLICK:
-                printf("%s", "DC");
-                printf("%d,", TMKEV_MOUSE_GET_X_COORD(l));
-                printf("%d", TMKEV_MOUSE_GET_Y_COORD(l));
+            case VK_RBUTTON:
+                printf("RB: %d,%d,", TMKEV_MOUSE_GET_X_COORD(l), TMKEV_MOUSE_GET_Y_COORD(l));
                 break;
-            case MOUSE_WHEELED:
-                printf("%s", "Wheel");
+            case VK_MBUTTON:
+                printf("MB: %d,%d,", TMKEV_MOUSE_GET_X_COORD(l), TMKEV_MOUSE_GET_Y_COORD(l));
                 break;
             default:
-                switch (TMKEV_GET_MOUSE_BUTTON_STATUS(w)) {
-                case VK_LBUTTON:
-                    printf("%s", "LB");
-                    printf("%d,", TMKEV_MOUSE_GET_X_COORD(l));
-                    printf("%d", TMKEV_MOUSE_GET_Y_COORD(l));
-                    break;
-                case VK_RBUTTON:
-                    printf("%s", "RB");
-                    printf("%d,", TMKEV_MOUSE_GET_X_COORD(l));
-                    printf("%d", TMKEV_MOUSE_GET_Y_COORD(l));
-                    break;
-                case VK_MBUTTON:
-                    printf("%s", "MB");
-                    printf("%d,", TMKEV_MOUSE_GET_X_COORD(l));
-                    printf("%d", TMKEV_MOUSE_GET_Y_COORD(l));
-                    break;
-                default:
-                    ;
-                }
-
+                ;
+            }
         }
     }
     else {
